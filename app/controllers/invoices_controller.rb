ed7d346b301934a809378a231706e4325fd23b44
Aalok <aalokdevk29@gmail.com>
2021-10-14 11:52:16 +0530

    Implement create invoice feature

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,6 +5,20 @@ class InvoicesController < ApplicationController
     @invoices = Invoice.all
   end
 
+  def new
+    @invoice = Invoice.new
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save
+      redirect_to invoices_path, notice: "Invoice for #{@invoice.client_name} successfully created."
+    else
+      render :new
+    end
+  end
+
   def destroy
     @invoice.destroy!
 
@@ -28,5 +42,9 @@ class InvoicesController < ApplicationController
 
   def find_invoice
     @invoice = Invoice.find params[:id]
+  end
+
+  def invoice_params
+     params.require(:invoice).permit(:client_name, :amount, :tax)
   end
 end
